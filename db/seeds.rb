9f14a1122e5a43313c0ed51f6c863c98a810eb9f
Marine Sourin <marinesourin@live.fr>
2019-02-14 17:14:15 +0100

    Back done, front started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint leaves")
+Ingredient.create(name: "Light rum")
+Ingredient.create(name: "Applejack")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Dark rum")
+Ingredient.create(name: "Sweet vermouth")
+Ingredient.create(name: "Strawberry schnapps")
+Ingredient.create(name: "Scotch")
+Ingredient.create(name: "Champagne")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Cranberry juice")
+Ingredient.create(name: "Pineapple juice")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Grenadine")
+Ingredient.create(name: "Ginger")
+Ingredient.create(name: "Pisco")
+Ingredient.create(name: "Lemonade")
+Ingredient.create(name: "Cachaca")
+
